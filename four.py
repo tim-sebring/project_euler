3894c804683ef587f59faeb1f31adb5b6ad5df5b
Tim Sebring <tim.sebring@gmail.com>
2016-11-30 15:36:23 -0500

    added four.py, can't import package with just a number as a name

---
+++ four.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import math
+
+# Project euler #4 - projecteuler.net
+
+
+def to_string(x):
+    """
+    Converts whatever is passed to it to a string, and returns
+    that value
+    """
+
+    return str(x)
+
+
+def reverse(x):
+    """
+    Returns the reverse value of whatever string is passed to it
+    """
+
+    return x[::-1]
+
+
+
+
+
